cbaff97ccfca60cdca722f4a3dfe531bde315aa4
Diego Reidel <diegoreidel@gmail.com>
2019-10-07 23:31:06 -0300

    Add very dummy 404 page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-card-body>
+        <b-card-header>
+            <p>404! We are sorry!</p>
+        </b-card-header>
+        <b-card-body>We are not able to fulfill your request</b-card-body>
+    </b-card-body>
+</template>
+
+<script>
+    export default {
+        name: 'PageNotFound'
+    }
+</script>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import AccountsList from '@/components/AccountsList'
 import AccountForm from '@/components/AccountForm'
-
+import PageNotFound from '@/components/PageNotFound'
 
 Vue.use(Router)
 
@@ -24,6 +24,11 @@ export default new Router({
       path: '/accounts/add',
       name: 'add-accounts',
       component: AccountForm
+    },
+    {
+      path: '**',
+      name: 'page-not-found',
+      component: PageNotFound
     }
   ]
 })
